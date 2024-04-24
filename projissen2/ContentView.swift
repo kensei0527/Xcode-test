63817f634ebb12c088ed2b6148ccc0796756ca47
kensei <kensei2680527@icloud.com>
2024-04-24 10:57:56 +0900

    Initial Commit

---
+++ projissen2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projissen2
+//
+//  Created by 古家健成 on 2024/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projissen2/projissen2App.swift
@@ -0,0 +1,17 @@
+//
+//  projissen2App.swift
+//  projissen2
+//
+//  Created by 古家健成 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct projissen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
